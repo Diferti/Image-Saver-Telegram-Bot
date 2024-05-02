6024adc0ebcaa821ec14f337b3a76bca7c521128
Dmitrii Semenov <dima.cemenov70@yandex.ru>
2024-05-01 20:41:26 -0400

    Added some functions

--- ImageSaverTelegramBot/main.py
+++ ImageSaverTelegramBot/main.py
@@ -9,6 +9,8 @@ token=""
 bot=telebot.TeleBot(token)
 
 users_directories=[]
+newDirectoryStatus=False
+sendPictureStatus=False
 
 # Main button menu for interacting with the bot
 def keyboard():
@@ -26,6 +28,13 @@ def secondKeyboard():
     directoryMenu.row(button1, button2)
     return directoryMenu
 
+def exitKeyboard():
+    exit = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton("Back")
+    button2 = types.KeyboardButton("Stop bot")
+    exit.row(button1, button2)
+    return exit
+
 @bot.message_handler(commands=['start'])
 def start(message):
     menu = keyboard()
@@ -33,6 +42,26 @@ def start(message):
                      text="<b>Hi, {0.first_name}!</b>\nI am the <i>Image Saver Bot</i>, I'll help you as much as I can to save your photos!\n"
                           "<u>Feel free to use buttons to interact with me!</u>".format(message.from_user),
                      parse_mode='html', reply_markup=menu)
+
+@bot.message_handler(func=lambda message: message.text == 'Back')
+def back(message):
+    global newDirectoryStatus, sendPictureStatus
+    newDirectoryStatus = False
+    sendPictureStatus = False
+    mainMenu = keyboard()
+    bot.send_message(message.chat.id,
+                     text="<b>Back to main menu</b>".format(message.from_user),
+                     parse_mode='html', reply_markup=mainMenu)
+
+@bot.message_handler(func=lambda message: message.text == 'Stop bot')
+def stop_bot(message):
+    global newDirectoryStatus, sendPictureStatus
+    newDirectoryStatus = False
+    sendPictureStatus = False
+    hideMarkup = types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id,
+                     text="<b>Bot stopped. To restart, send /start command</b>".format(message.from_user),
+                     parse_mode='html', reply_markup=hideMarkup)
 
 @bot.message_handler(func=lambda message: message.text == 'Save photos')
 def savePhotos(message):
@@ -52,13 +81,42 @@ def saveByFolders(message):
 
 @bot.message_handler(func=lambda message: message.text == 'New directory path')
 def changeDirectory(message):
+    global newDirectoryStatus
     hideMarkup = types.ReplyKeyboardRemove()
     bot.send_message(message.chat.id,
-                     text="<b>Send directory path</b> like this: \n<b><i>C:\\Users\\User123\\Downloads</i></b>!".format(message.from_user),
+                     text="<b>Send directory path</b> like this: \n<b><i>C:\\Users\\User123\\Downloads</i></b>".format(message.from_user),
                      parse_mode='html', reply_markup=hideMarkup)
+    newDirectoryStatus = True
+
+@bot.message_handler(func=lambda message: message.text == 'Leave previous one')
+def leaveDirectory(message):
+    global newDirectoryStatus, sendPictureStatus
+    user_id=message.chat.id
+    exitMarkup = exitKeyboard()
+    directory_path=None
+
+    for user_directory in users_directories:
+        if user_directory['user_id'] == user_id:
+            directory_path=user_directory['path']
+            break
+
+    if directory_path is None:
+        directoryMenu = secondKeyboard()
+        bot.send_message(user_id, "You need to set your directory path first.", reply_markup=directoryMenu)
+        return
+
+    bot.send_message(user_id, text=f"<b>Directory path is left as:</b> \n<b><i>{directory_path}</i></b>"
+                          "\n\nNow send your images that you want to save!", parse_mode='html', reply_markup=exitMarkup)
+    newDirectoryStatus = False
+    sendPictureStatus = True
 
 @bot.message_handler(func=lambda message: True)
 def changeDirectoryPath(message):
+    global newDirectoryStatus, sendPictureStatus
+    if not newDirectoryStatus:
+        return
+
+    exitMarkup = exitKeyboard()
     user_id = message.chat.id
     new_directory = message.text
 
@@ -68,17 +126,26 @@ def changeDirectoryPath(message):
             user_directory['path'] = new_directory
             bot.send_message(user_id,
                              text=f"<b>Directory path changed successfully to:</b> \n<b><i>{new_directory}</i></b>"
-                             "\n\nNow send your images that you want to save!", parse_mode='html')
+                             "\n\nNow send your images that you want to save!", parse_mode='html', reply_markup=exitMarkup)
+            newDirectoryStatus = False
+            sendPictureStatus = True
             return
 
     # If user_id is not found in users_directories, add it
     users_directories.append({'user_id': user_id, 'path': new_directory})
     bot.send_message(user_id,
                      text=f"<b>Directory path set successfully to:</b> \n<b><i>{new_directory}</i></b>"
-                     "\n\nNow send your images that you want to save!",parse_mode='html')
+                     "\n\nNow send your images that you want to save!", parse_mode='html', reply_markup=exitMarkup)
+    newDirectoryStatus = False
+    sendPictureStatus = True
 
 @bot.message_handler(content_types=['photo'])
 def save_image(message):
+    global sendPictureStatus
+    if not sendPictureStatus:
+        return
+
+    exitMarkup = exitKeyboard()
     file_info = bot.get_file(message.photo[-1].file_id)
     file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
 
@@ -92,13 +159,15 @@ def save_image(message):
             break
 
     if directory_path is None:
-        bot.send_message(user_id, "You need to set your directory path first.")
+        directoryMenu = secondKeyboard()
+        bot.send_message(user_id, "You need to set your directory path first.", reply_markup=directoryMenu)
+        sendPictureStatus = False
         return
 
     # Save the image to the directory
     with open(os.path.join(directory_path, f"{message.photo[-1].file_id}.jpg"), 'wb') as new_file:
         new_file.write(file.content)
 
-    bot.send_message(user_id, "Image saved successfully!")
+    bot.send_message(user_id, "Image saved successfully!", reply_markup=exitMarkup)
 
 bot.polling(none_stop=True)
