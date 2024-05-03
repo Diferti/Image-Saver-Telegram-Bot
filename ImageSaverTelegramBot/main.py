6a2ffb28a319ebb555ace6a8f272bf575e708859
Dmitrii Semenov <dima.cemenov70@yandex.ru>
2024-05-02 23:11:36 -0400

    It works, but need more tests

--- ImageSaverTelegramBot/main.py
+++ ImageSaverTelegramBot/main.py
@@ -11,6 +11,7 @@ bot=telebot.TeleBot(token)
 users_directories=[]
 newDirectoryStatus=False
 sendPictureStatus=False
+saveByFoldersStatus=False
 
 # Main button menu for interacting with the bot
 def keyboard():
@@ -45,9 +46,10 @@ def start(message):
 
 @bot.message_handler(func=lambda message: message.text == 'Back')
 def back(message):
-    global newDirectoryStatus, sendPictureStatus
+    global newDirectoryStatus, sendPictureStatus, saveByFoldersStatus
     newDirectoryStatus = False
     sendPictureStatus = False
+    saveByFoldersStatus = False
     mainMenu = keyboard()
     bot.send_message(message.chat.id,
                      text="<b>Back to main menu</b>".format(message.from_user),
@@ -55,9 +57,10 @@ def back(message):
 
 @bot.message_handler(func=lambda message: message.text == 'Stop bot')
 def stop_bot(message):
-    global newDirectoryStatus, sendPictureStatus
+    global newDirectoryStatus, sendPictureStatus, saveByFoldersStatus
     newDirectoryStatus = False
     sendPictureStatus = False
+    saveByFoldersStatus = False
     hideMarkup = types.ReplyKeyboardRemove()
     bot.send_message(message.chat.id,
                      text="<b>Bot stopped. To restart, send /start command</b>".format(message.from_user),
@@ -73,11 +76,13 @@ def savePhotos(message):
 
 @bot.message_handler(func=lambda message: message.text == 'Save by folders')
 def saveByFolders(message):
+    global saveByFoldersStatus
     directoryMenu = secondKeyboard()
     bot.send_message(message.chat.id,
                      text="<b>Save by folders option</b> will save your images that you sent to <b><i>new folders</i></b>!\n"
-                          "<u>Before send your images make sure that you set up folder path!</u>".format(message.from_user),
+                          "<u>Before send your images/videos make sure that you set up folder path!</u>".format(message.from_user),
                      parse_mode='html', reply_markup=directoryMenu)
+    saveByFoldersStatus = True
 
 @bot.message_handler(func=lambda message: message.text == 'New directory path')
 def changeDirectory(message):
@@ -90,7 +95,7 @@ def changeDirectory(message):
 
 @bot.message_handler(func=lambda message: message.text == 'Leave previous one')
 def leaveDirectory(message):
-    global newDirectoryStatus, sendPictureStatus
+    global newDirectoryStatus, sendPictureStatus, saveByFoldersStatus
     user_id=message.chat.id
     exitMarkup = exitKeyboard()
     directory_path=None
@@ -105,14 +110,19 @@ def leaveDirectory(message):
         bot.send_message(user_id, "You need to set your directory path first.", reply_markup=directoryMenu)
         return
 
-    bot.send_message(user_id, text=f"<b>Directory path is left as:</b> \n<b><i>{directory_path}</i></b>"
-                          "\n\nNow send your images that you want to save!", parse_mode='html', reply_markup=exitMarkup)
+    if saveByFoldersStatus:
+        bot.send_message(user_id, text=f"<b>Directory path is left as:</b> \n<b><i>{directory_path}</i></b>"
+                         "\n\nNow send name of new folder and then your images/videos that you want to save!", parse_mode='html',
+                         reply_markup=exitMarkup)
+    else:
+        bot.send_message(user_id, text=f"<b>Directory path is left as:</b> \n<b><i>{directory_path}</i></b>"
+                        "\n\nNow send your images/videos that you want to save!", parse_mode='html', reply_markup=exitMarkup)
     newDirectoryStatus = False
     sendPictureStatus = True
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: newDirectoryStatus)
 def changeDirectoryPath(message):
-    global newDirectoryStatus, sendPictureStatus
+    global newDirectoryStatus, sendPictureStatus, saveByFoldersStatus
     if not newDirectoryStatus:
         return
 
@@ -124,29 +134,87 @@ def changeDirectoryPath(message):
     for user_directory in users_directories:
         if user_directory['user_id'] == user_id:
             user_directory['path'] = new_directory
-            bot.send_message(user_id,
-                             text=f"<b>Directory path changed successfully to:</b> \n<b><i>{new_directory}</i></b>"
-                             "\n\nNow send your images that you want to save!", parse_mode='html', reply_markup=exitMarkup)
+            if saveByFoldersStatus:
+                bot.send_message(user_id, text=f"<b>Directory path changed successfully to:</b> \n<b><i>{new_directory}</i></b>"
+                                 "\n\nNow send name of new folder and then your images/videos that you want to save!", parse_mode='html',
+                                 reply_markup=exitMarkup)
+            else:
+                bot.send_message(user_id, text=f"<b>Directory path changed successfully to:</b> \n<b><i>{new_directory}</i></b>"
+                                "\n\nNow send your images/videos that you want to save!", parse_mode='html', reply_markup=exitMarkup)
+
             newDirectoryStatus = False
             sendPictureStatus = True
             return
 
     # If user_id is not found in users_directories, add it
-    users_directories.append({'user_id': user_id, 'path': new_directory})
-    bot.send_message(user_id,
-                     text=f"<b>Directory path set successfully to:</b> \n<b><i>{new_directory}</i></b>"
-                     "\n\nNow send your images that you want to save!", parse_mode='html', reply_markup=exitMarkup)
+    users_directories.append({'user_id': user_id, 'path': new_directory, 'newFolder': None})
+    if saveByFoldersStatus:
+        bot.send_message(user_id, text=f"<b>Directory path changed successfully to:</b> \n<b><i>{new_directory}</i></b>"
+                                       "\n\nNow send name of new folder and then your images/videos that you want to save!",
+                         parse_mode='html',
+                         reply_markup=exitMarkup)
+    else:
+        bot.send_message(user_id, text=f"<b>Directory path changed successfully to:</b> \n<b><i>{new_directory}</i></b>"
+                                       "\n\nNow send your images/videos that you want to save!", parse_mode='html',
+                         reply_markup=exitMarkup)
     newDirectoryStatus = False
     sendPictureStatus = True
 
-@bot.message_handler(content_types=['photo'])
-def save_image(message):
-    global sendPictureStatus
+@bot.message_handler(func=lambda message: saveByFoldersStatus and not newDirectoryStatus)
+def newFolder(message):
+    global saveByFoldersStatus, newDirectoryStatus, sendPictureStatus
+    if not saveByFoldersStatus or newDirectoryStatus:
+        return
+
+    user_id = message.chat.id
+    folder = message.text.strip()
+    directory_path = None
+
+    # Find the directory path for the user
+    for user_directory in users_directories:
+        if user_directory['user_id'] == user_id:
+            directory_path = user_directory['path']
+            # Construct new path for the new folder
+            new_directory = os.path.join(directory_path, folder)
+            user_directory['newFolder'] = new_directory
+            break
+
+    if directory_path is None:
+        mainMenu = keyboard()
+        bot.send_message(user_id, "You need to set your directory path first.", reply_markup=mainMenu)
+        sendPictureStatus = False
+        saveByFoldersStatus = False
+        return
+
+    # Check if the new folder already exists
+    if os.path.exists(new_directory):
+        bot.send_message(user_id, "Directory already exists.")
+    else:
+        try:
+            # Create the new folder
+            os.makedirs(new_directory)
+            bot.send_message(user_id, f"New folder created: {new_directory}")
+        except Exception as e:
+            bot.send_message(user_id, f"Error creating directory: {e}")
+
+@bot.message_handler(content_types=['photo', 'video'])
+def save_image_video(message):
+    global sendPictureStatus, saveByFoldersStatus
     if not sendPictureStatus:
         return
 
     exitMarkup = exitKeyboard()
-    file_info = bot.get_file(message.photo[-1].file_id)
+
+    if message.content_type == 'photo':
+        media_type = 'jpg'
+        file_id = message.photo[-1].file_id
+    elif message.content_type == 'video':
+        media_type = 'mp4'
+        file_id = message.video.file_id
+    else:
+        return
+
+    file_info = bot.get_file(file_id)
     file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
 
     user_id = message.chat.id
@@ -155,19 +223,26 @@ def save_image(message):
     # Find the directory path for the user
     for user_directory in users_directories:
         if user_directory['user_id'] == user_id:
-            directory_path = user_directory['path']
+            if user_directory['newFolder'] is not None:
+                directory_path = user_directory['newFolder']
+            else:
+                directory_path = user_directory['path']
             break
 
     if directory_path is None:
-        directoryMenu = secondKeyboard()
-        bot.send_message(user_id, "You need to set your directory path first.", reply_markup=directoryMenu)
+        mainMenu = keyboard()
+        bot.send_message(user_id, "You need to set your directory path first.", reply_markup=mainMenu)
         sendPictureStatus = False
+        saveByFoldersStatus = False
         return
 
-    # Save the image to the directory
-    with open(os.path.join(directory_path, f"{message.photo[-1].file_id}.jpg"), 'wb') as new_file:
+    # Count existing files to determine the next file name
+    file_count = len([name for name in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, name))])
+
+    # Save the content to the directory with a sequential file name
+    with open(os.path.join(directory_path, f"{file_count + 1}.{media_type}"), 'wb') as new_file:
         new_file.write(file.content)
 
-    bot.send_message(user_id, "Image saved successfully!", reply_markup=exitMarkup)
+    bot.send_message(user_id, f"File №{file_count + 1} saved successfully!", reply_markup=exitMarkup)
 
 bot.polling(none_stop=True)
